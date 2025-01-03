13ebb26bdc3aa0a7ac1e611e2f5d10e9922aa8c9
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-03 08:26:07 +0100

    Update Python QuickStart for People Learning AI.py

--- Python QuickStart for People Learning AI.py
+++ Python QuickStart for People Learning AI.py
@@ -5,3 +5,23 @@
 
 # This lesson is created by Sha Talebi. This link is for original source: https://youtu.be/pNg2DJ4spXg?si=XWMjtiCOuiDzBToq
 
+# Introduction
+
+# What is Python (Programming Language)?
+# Python is a programming language which is simply a way to give computers precise instructions to do things we can't or don't want to do. 
+
+# Coding is Easier Than Ever - Thanks to Google and ChatGPT
+
+# This tutorial is for Who
+  # learning AI + some coding, but new to Python
+
+# Python Basics
+
+# Data Types
+    # A way to classify data to process it appropriately and efficiently
+# Text 
+      # Strings = chracter sequences e.g. "Hello", "He11o!"
+         a = 'This is a tring' # no difference between ' and " quotation nut
+         b = 'Zafar said "Python is awesome!"' # here " may cause an error 
+         c = """this is 
+             a string"""                      # we can use """ quotation for multiple lines of strings
