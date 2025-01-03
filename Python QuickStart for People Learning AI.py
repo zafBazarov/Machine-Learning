09a72dfdb6a261b43b6f5102d20b22c9c2cff3d9
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-03 08:10:01 +0100

    Update Python QuickStart for People Learning AI.py
    This content gives a quick start guide for python. For orignal source please use this link: https://youtu.be/pNg2DJ4spXg?si=XWMjtiCOuiDzBToq

--- Python QuickStart for People Learning AI.py
+++ Python QuickStart for People Learning AI.py
@@ -1,1 +1,7 @@
 
+# Python for People Learning AI [Mini-Course]
+
+# Beginner friendly- QuickStart Guide.
+
+# This lesson is created by Sha Talebi. This link is for original source: https://youtu.be/pNg2DJ4spXg?si=XWMjtiCOuiDzBToq
+
