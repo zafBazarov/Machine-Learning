97b2db12ee40f08149cb14b2bad12093ad18b385
Zafarali Bazarov <113635785+zafBazarov@users.noreply.github.com>
2025-01-03 08:42:54 +0100

    Update Python QuickStart for People Learning AI.py

--- Python QuickStart for People Learning AI.py
+++ Python QuickStart for People Learning AI.py
@@ -23,5 +23,20 @@
       # Strings = chracter sequences e.g. "Hello", "He11o!"
          a = 'This is a tring' # no difference between ' and " quotation nut
          b = 'Zafar said "Python is awesome!"' # here " may cause an error 
-         c = """this is 
-             a string"""                      # we can use """ quotation for multiple lines of strings
+
+# Numbers
+        # Ints = integers e.g. 1,2, 42
+          integer = 1
+        # Floats = numbers with decimals e.g. 1.0, 1.34
+          float = 3.41
+
+# Lists = ordered collection of values
+          lst1 = ['a', 'b', 'c']  # a list of strings
+
+          lst2 = [1, 2, 3]        # a list of ints
+
+          lst3 = ["a", 1, 3.14]   # list with string, int and float
+
+          lst4= [["a","b"], [1, 2], [2.2, 3.3]] # list of lists
+
+# Dictionaries = key-value pair sequences
